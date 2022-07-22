bbb21ce80c477075f960f24a91a9bf1c313d0d69
emmanuel saura <emmanuelsaura007@gmail.com>
2022-07-22 05:52:02 -0700

    my commit

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
 
 def print_last_digit(number):
-    return number[-1]
+    if number < 0:
+        last_digit = ((number * -1) % 10)
+    else:
+        last_digit = number % 10
+    print("{}".format(last_digit), end="")
+    return last_digit
